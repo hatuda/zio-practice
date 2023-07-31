4ab35661278cee4fb989b5d30417668598fc7580
yutaro.hatsuda <yutaro.hatsuda@webcrew.co.jp>
2023-07-31 14:07:35 +0900

    fix:ファイル名等について修正

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -3,5 +3,5 @@ package jp.webcrew.hands.on.zio
 import zio._
 
 object MainApp extends ZIOAppDefault {
-  def run = Console.printLine("Hello, World!")
+  def run: ZIO[Any, Throwable, Unit] = Console.printLine("Hello, World!")
 }
