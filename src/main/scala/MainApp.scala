35541d474f0ca3bf1cb8bed4ca3c3e125ed389f4
yutaro.hatsuda <yutaro.hatsuda@webcrew.co.jp>
2023-08-14 18:32:55 +0900

    mod:意図的に例外を発生させるように修正

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -12,7 +12,8 @@ object MainApp extends ZIOAppDefault {
   def run: ZIO[Any, Throwable, Unit] = {
     import java.text.SimpleDateFormat
     // 任意の日付文字列
-    val inpDateStr = "2023/07/25 17:46:00"
+    // 意図的に例外を発生させる文字列に変更
+    val inpDateStr = "2023/07/aa 17:46:00"
     // 取り扱う日付の形にフォーマット設定
     val sdformat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
     // Date型に変換( DateFromatクラスのparse() )
