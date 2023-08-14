48da489d6e19b3e58a1439351a10cc5e5244e94b
yutaro.hatsuda <yutaro.hatsuda@webcrew.co.jp>
2023-08-14 18:01:58 +0900

    add:failure例

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -2,18 +2,33 @@ package jp.webcrew.hands.on.zio
 
 import application.service.ApplicationService
 import application.service.impl.ApplicationServiceImpl
+
 import zio.*
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
 object MainApp extends ZIOAppDefault {
-  def run: ZIO[Any, Throwable, Unit] = ApplicationService.consoleOutput().provide(ZLayer.fromZIO(ZIO.attempt {
-    import java.text.SimpleDateFormat
-    // 任意の日付文字列
-    val inpDateStr = "2023/07/25 17:46:00"
+  def run: ZIO[Any, Throwable, Unit] = {
+    for {
+      date <- ZIO.attempt {
+        import java.text.SimpleDateFormat
+        // 任意の日付文字列
+        val inpDateStr = "2023/07/aa 17:46:00"
+        // 取り扱う日付の形にフォーマット設定
+        val sdformat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+        // Date型に変換( DateFromatクラスのparse() )
+        val date = sdformat.parse(inpDateStr)
+        date
+      }
+      _ <- consoleOutput(date)
+    } yield ()
+  }.catchAllCause(cause => Console.printLine(cause))
 
-    // 取り扱う日付の形にフォーマット設定
-    val sdformat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
-
-    // Date型に変換( DateFromatクラスのparse() )
-    sdformat.parse(inpDateStr)
-  }), ApplicationServiceImpl.layer)
+  def consoleOutput(date: Date): ZIO[Any, Throwable, Unit] =
+    for {
+      _ <- Console.printLine(
+        s"${new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(date)} Hello, World!"
+      )
+    } yield ()
 }
