6b077e1a47fd62e3d7f5e76045d811bd44e1c7c2
yutaro.hatsuda <yutaro.hatsuda@webcrew.co.jp>
2023-09-20 18:30:14 +0900

    mod:エラー処理について修正

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,18 +1,18 @@
 package jp.webcrew.hands.on.zio
 
+import io.netty.handler.codec.http.HttpHeaderNames
 import jp.webcrew.hands.on.zio.application.service.impl.ApplicationServiceImpl
 import jp.webcrew.hands.on.zio.presentation.controller.TestController
 import zio.*
-import zio.http._
+import zio.http.*
 
 object MainApp extends ZIOAppDefault {
 
-
   override val run: ZIO[Any, Throwable, Nothing] =
     Server
-      .serve(TestController().mapError { _ =>
+      .serve(TestController().catchAllCauseZIO { _ =>
         // エラー処理
-        Response(status = Status.InternalServerError)
+        ZIO.succeed(Response(status = Status.InternalServerError))
       })
       .provide(
         Server.default,
