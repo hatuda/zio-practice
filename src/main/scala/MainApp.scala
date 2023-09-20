e66ef70784cbb761b11b36412f8f2d6ebaaf9779
yutaro.hatsuda <yutaro.hatsuda@webcrew.co.jp>
2023-09-20 16:45:32 +0900

    add:GETメソッド例追加

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,19 +1,15 @@
 package jp.webcrew.hands.on.zio
 
-import application.service.ApplicationService
-import application.service.impl.ApplicationServiceImpl
-import zio.*
+import zio._
+import zio.http._
 
 object MainApp extends ZIOAppDefault {
-  def run: ZIO[Any, Throwable, Unit] = ApplicationService.consoleOutput().provide(ZLayer.fromZIO(ZIO.attempt {
-    import java.text.SimpleDateFormat
-    // 任意の日付文字列
-    val inpDateStr = "2023/07/25 17:46:00"
 
-    // 取り扱う日付の形にフォーマット設定
-    val sdformat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+  val app: App[Any] =
+    Http.collect[Request] {
+      case Method.GET -> Root / "text" => Response.text("Hello World!")
+    }
 
-    // Date型に変換( DateFromatクラスのparse() )
-    sdformat.parse(inpDateStr)
-  }), ApplicationServiceImpl.layer)
+  override val run =
+    Server.serve(app).provide(Server.default)
 }
