9da9a8a15a7593a2c48b955287a6ff8c32a7e11c
yutaro.hatsuda <yutaro.hatsuda@webcrew.co.jp>
2023-09-20 13:39:06 +0900

    add:並列実行の前の直列実行確認

--- src/main/scala/application/service/impl/ApplicationServiceImpl.scala
+++ src/main/scala/application/service/impl/ApplicationServiceImpl.scala
@@ -9,16 +9,7 @@ import java.util.Date
 
 case class ApplicationServiceImpl(currentDate: Date) extends ApplicationService {
   override def consoleOutput(): ZIO[Any, Throwable, Unit] = for {
-    fiber <- (for {
-      _ <- ZIO.attempt(Thread.sleep(3000))
-      _ <- Console.printLine("並列処理実行")
-      result <- ZIO.succeed("並列処理:成功")
-    } yield result).fork
-    _ <- Console.printLine(
-      s"${new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(currentDate)} Hello, World!"
-    )
-    result <- fiber.join
-    _ <- Console.printLine(result)
+    _ <- ZIO.foreach(List.range(1, 100))(n => Console.printLine(n))
   } yield ()
 }
 
