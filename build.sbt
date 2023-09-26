d3b6de223bd0d8f9aa4d97f27c32ce13634d3f71
yutaro.hatsuda <yutaro.hatsuda@webcrew.co.jp>
2023-09-26 12:24:18 +0900

    add:既存のコードに対するテスト例を追加

--- build.sbt
+++ build.sbt
@@ -6,5 +6,12 @@ lazy val root = (project in file("."))
   .settings(
     name                             := "zio-hands-on",
     idePackagePrefix                 := Some("jp.webcrew.hands.on.zio"),
-    libraryDependencies += "dev.zio" %% "zio-http" % "3.0.0-RC2"
+    libraryDependencies += "dev.zio" %% "zio-http" % "3.0.0-RC2",
+    libraryDependencies ++= Seq(
+      "dev.zio" %% "zio-test"          % "2.0.17" % Test,
+      "dev.zio" %% "zio-test-sbt"      % "2.0.17" % Test,
+      "dev.zio" %% "zio-test-magnolia" % "2.0.17" % Test
+    ),
+    testFrameworks +=
+      new TestFramework("zio.test.sbt.ZTestFramework")
   )
---
+++ src/test/scala/TrainingSpec.scala
@@ -0,0 +1,30 @@
+package jp.webcrew.hands.on.zio
+
+import application.service.ApplicationService
+import application.service.impl.ApplicationServiceImpl
+import zio.*
+import zio.http.{Body, Client, Method, Request, URL}
+import zio.test.*
+import zio.test.Assertion.*
+
+object TrainingSpec extends ZIOSpecDefault {
+  def spec: Spec[Any, Throwable] = suite("HelloWorldSpec")(
+    test("sayHello correctly displays output") {
+      for {
+        test <- ZIO.succeed("test")
+        uri  <- ZIO.fromEither(URL.decode("http://localhost:8080/postTest"))
+        res <- ApplicationService.postTest(
+          Request
+            .default(
+              Method.POST,
+              uri,
+              Body.fromString(
+                s"userName=$test"
+              )
+            )
+        )
+        bodyString <- res.body.asString
+      } yield assertTrue(bodyString == s"あなたの名前は${test}です。")
+    }.provide(ApplicationServiceImpl.layer, Client.default)
+  )
+}
